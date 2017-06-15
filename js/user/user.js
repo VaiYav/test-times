1a773fc0371d9368f80aaf2f306bfab22cf54e09
VaiYav <yakovlev.valentin97@gmail.com>
2017-06-15 21:23:30 +0300

    section_price

--- js/user/user.js
+++ js/user/user.js
@@ -12,3 +12,17 @@
     	document.getElementById("myNav").style.height = "0%";
     	document.querySelector(".bar-container").classList.toggle("change");
     }
+    function hoverButton() {
+    	var title = document.querySelectorAll('.price-container-title');
+    	var button = document.querySelectorAll('.price-container-button');
+    	for (let a = 0; a < button.length; a++) {
+    		button[a].onmouseover = function () {
+    			title[a].style.background = '#49cbcd';
+    			button[a].style.background = '#49cbcd'
+    		}
+    		button[a].onmouseout = function () {
+    			title[a].style.background = '#485460';
+    			button[a].style.background = '#788492'
+    		}
+    	}
+    }
